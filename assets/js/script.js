724128381a230be9f89e1d2b2a5dd6082e417007
DevAngelo98 <ainglima07@gmail.com>
2019-10-24 10:16:50 +0200

    Posizione in lista ordinata

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-var listaCognomi = ["Inglima", "Carusotto", "Gallo"]
+var listaCognomi = ["Inglima", "Carusotto", "Gallo"];
 var cognomeInserito = prompt("Inserisci il tuo cognome");
 var precedente;
 var presente = false;
+var pos;
 
 //Stampo i cognomi non in ordine
 document.getElementById("listanon").innerHTML = "<li>" + "Lista non ordinata: " + "</li>" + "<br>";
 for(var i=0; i<listaCognomi.length; i++){
 
-  if(cognomeInserito == listaCognomi[i]){
-    presente = true;
-  }
-
   precedente = document.getElementById("listanon").innerHTML;
   document.getElementById("listanon").innerHTML = precedente + "<li>" + listaCognomi[i] + "</li>";
 
+  if(cognomeInserito == listaCognomi[i]){
+    presente = true;
+  }
 }
 
 //Verifico la presenza del cognome inserito 
@@ -21,15 +21,20 @@ if(presente==false){
   listaCognomi.push(cognomeInserito);
 }
 
+listaCognomi.sort();
+
 
 //Stampo cognomi ordinati 
 document.getElementById("listaord").innerHTML = "<li>" + "Lista ordinata: " + "</li>" + "<br>";
-listaCognomi.sort();
 for(var i=0; i<listaCognomi.length; i++){
   precedente = document.getElementById("listaord").innerHTML;
   document.getElementById("listaord").innerHTML = precedente + "<li>" + listaCognomi[i] + "</li>";
+
+  if(listaCognomi[i] == cognomeInserito){
+    pos = i+1;
+  }
+
 }
 
-
-
-
+document.getElementById("pos").innerHTML = "<li>" + "Posizione del cognome inserito nella lista ordinata:" + "</li>" + "<br>";
+document.getElementById("pos").innerHTML += "<li>" + (pos) + "</li>";
